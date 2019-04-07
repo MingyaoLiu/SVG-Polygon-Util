85b923e6487a4018620aa6e060a5156de2881b61
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2019-04-06 21:41:36 -0400

    improve layout, change data structure

--- SVG-Poly-Util/src/app/app.component.ts
+++ SVG-Poly-Util/src/app/app.component.ts
@@ -2,64 +2,116 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit {
 
     constructor() { }
 
     polyObj = {
-        canvasWidth: 400,
-        canvasHeight: 200,
-        edgeCount: 3,
-        width: 10,
-        height: 3,
-        rotationDeg: 30,
-        strokeWidth: 3,
-        rotate: false,
-        transformOriginY: 0,
+        canvasWidth: {
+            label: 'Canvas Width',
+            inputType: 'number',
+            value: 400
+        },
+        canvasHeight: {
+            label: 'Canvas Height',
+            inputType: 'number',
+            value: 200
+        },
+        edgeCount: {
+            label: 'Edge Count',
+            inputType: 'number',
+            value: 5
+        },
+        strokeWidth: {
+            label: 'Stroke Width',
+            inputType: 'number',
+            value: 2
+        },
+        strokeColor: {
+            label: 'Stroke Color',
+            inputType: 'color',
+            value: '#EFEFEF'
+        },
+        fillColor: {
+            label: 'Fill Color',
+            inputType: 'color',
+            value: '#BCBCBC'
+        },
+        canvasColor: {
+            label: 'Canvas Color',
+            inputType: 'color',
+            value: '#454545'
+        },
+        rotate: {
+            label: 'Rotate Poly',
+            inputType: 'checkbox',
+            value: true
+        },
+        rotationDeg: {
+            label: 'Rotate Transform Degree',
+            inputType: 'number',
+            value: 222
+        },
+        fitInAll: {
+            label: 'Fit Poly in Canvas',
+            inputType: 'checkbox',
+            value: true
+        },
+        transformOriginY: {
+            label: 'Transform Origin Y',
+            inputType: 'number',
+            value: 222
+        },
     };
-    rectPath = '0 0, 0 ' + this.polyObj.canvasHeight + ', ' + this.polyObj.canvasWidth + ' ' + this.polyObj.canvasHeight + ', ' + this.polyObj.canvasWidth + ' ' + 0;
+    polyObjArr = Object.keys(this.polyObj);
+
+
+    rectPath = '0 0, 0 ' + this.polyObj.canvasHeight.value + ', ' + this.polyObj.canvasWidth.value + ' ' + this.polyObj.canvasHeight.value + ', ' + this.polyObj.canvasWidth.value + ' ' + 0;
     generatedPoints = '';
 
     ngOnInit() {
         this.calculatePointsFromCenter();
+        console.log(this.polyObjArr);
     }
 
 
     onChange(event) {
-        this.rectPath = '0 0, 0 ' + this.polyObj.canvasHeight + ', ' + this.polyObj.canvasWidth + ' ' + this.polyObj.canvasHeight + ', ' + this.polyObj.canvasWidth + ' ' + 0;
+        this.rectPath = '0 0, 0 ' + this.polyObj.canvasHeight.value + ', ' + this.polyObj.canvasWidth.value + ' ' + this.polyObj.canvasHeight.value + ', ' + this.polyObj.canvasWidth.value + ' ' + 0;
         this.calculatePointsFromCenter();
         this.getTransformOrigin();
     }
 
 
     getTransformOrigin() {
-        return '' + this.polyObj.canvasWidth / 2 + 'px ' + this.polyObj.transformOriginY + 'px';
+        return '' + this.polyObj.canvasWidth.value / 2 + 'px ' + this.polyObj.transformOriginY.value + 'px';
     }
 
     calculatePointsFromCenter() {
 
-        const centerAngle = 2 / this.polyObj.edgeCount * Math.PI;
-
-        const r = this.polyObj.edgeCount % 2 === 0 ? (this.polyObj.canvasHeight - 2 * this.polyObj.strokeWidth) / 2 : (this.polyObj.canvasHeight - 2 * this.polyObj.strokeWidth) / (1 + Math.cos(centerAngle / 2));
-        this.polyObj.transformOriginY = r + this.polyObj.strokeWidth;
-        const centerX: number = this.polyObj.canvasWidth / 2;
-        const centerY: number = this.polyObj.edgeCount % 2 === 0 ? this.polyObj.canvasHeight / 2 : r + this.polyObj.strokeWidth;
-        let leftPointPath = '' + this.polyObj.canvasWidth / 2 + ' ' + this.polyObj.strokeWidth;
+        const centerAngle = 2 / this.polyObj.edgeCount.value * Math.PI;
+        let r = (this.polyObj.canvasHeight.value - 2 * this.polyObj.strokeWidth.value) / 2;
+        if (!this.polyObj.fitInAll && this.polyObj.edgeCount.value % 2 !== 0) {
+            r = (this.polyObj.canvasHeight.value - 2 * this.polyObj.strokeWidth.value) / (1 + Math.cos(centerAngle / 2));
+        }
+        this.polyObj.transformOriginY.value = r + this.polyObj.strokeWidth.value;
+        const centerX: number = this.polyObj.canvasWidth.value / 2;
+        const centerY: number = this.polyObj.edgeCount.value % 2 === 0 ? this.polyObj.canvasHeight.value / 2 : r + this.polyObj.strokeWidth.value;
+        let leftPointPath = '' + this.polyObj.canvasWidth.value / 2 + ' ' + this.polyObj.strokeWidth.value;
         let rightPointPath = '';
 
-        for (let i = 0; i < Math.floor((this.polyObj.edgeCount - 1) / 2); i++) {
+        for (let i = 0; i < Math.floor((this.polyObj.edgeCount.value - 1) / 2); i++) {
             const nextAngle = (i + 1) * centerAngle;
             const pointX = centerX - r * Math.sin(nextAngle);
             const pointY = centerY - r * Math.cos(nextAngle);
             leftPointPath += ', ' + pointX + ' ' + pointY;
             rightPointPath = ', ' + (centerX * 2 - pointX) + ' ' + pointY + '' + rightPointPath;
         }
-        if (this.polyObj.edgeCount % 2 === 0) {
-            leftPointPath += ', ' + this.polyObj.canvasWidth / 2 + ' ' + (this.polyObj.canvasHeight - this.polyObj.strokeWidth);
+        if (this.polyObj.edgeCount.value % 2 === 0) {
+            leftPointPath += ', ' + this.polyObj.canvasWidth.value / 2 + ' ' + (this.polyObj.canvasHeight.value - this.polyObj.strokeWidth.value);
         }
         this.generatedPoints = leftPointPath + rightPointPath;
     }
